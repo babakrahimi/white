72e747b995116732e1f0757b2a4d6a0d85a324d3
Amir Hossein Abdolkhalegh <ah.abdolkhalegh@gmail.com>
2017-12-18 00:06:42 +0330

    refactor router

--- app/router.go
+++ app/router.go
@@ -14,15 +14,17 @@ func NewRouter() *Router {
 }
 
 func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	bs := bytes.Buffer{}
-	e := json.NewEncoder(&bs)
-
 	switch r.RequestURI {
 	case "/api/restaurant":
-		e.Encode(GetRestaurants())
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		bs.WriteTo(w)
+		writeToJson(w, GetRestaurants())
 	}
+}
 
+func writeToJson(w http.ResponseWriter, data interface{}) {
+	bs := bytes.Buffer{}
+	e := json.NewEncoder(&bs)
+	e.Encode(data)
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	bs.WriteTo(w)
 }
