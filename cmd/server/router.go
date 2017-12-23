5143bfab46588fd07121c5b4697e90f5eb7bc7d5
Amir Hossein Abdolkhalegh <ah.abdolkhalegh@gmail.com>
2017-12-23 23:11:34 +0330

    add CORS functionality to the router

--- cmd/server/router.go
+++ cmd/server/router.go
@@ -18,7 +18,14 @@ func NewRouter() *Router {
 }
 
 func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	allowCORS(w)
 	router.handler.ServeHTTP(w, r)
+}
+
+func allowCORS(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 }
 
 func getHandler() http.Handler {
